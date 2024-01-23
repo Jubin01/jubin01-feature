397a146436f6de2cb6a313b0d17008b077bbf38f
Jubin01 <jubinmail@gmail.com>
2024-01-23 20:23:22 +0530

    embed tictactoe game (#2)
    Co-authored-by: jubin thomas <athom130@adient.com>

--- src/app.vue
+++ src/app.vue
@@ -4,7 +4,8 @@
     <nav>
      <!-- <router-link to="/">Home</router-link> | -->
       
-      <!-- <router-link to="/games">Games</router-link> | -->
+      <router-link to="/games">Games</router-link> |
+      <router-link to="/apps">Apps</router-link> 
     </nav>
     <router-view />
     <Footer />
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,33 @@
 import Vue from 'vue'
-import  VueRouter  from "vue-router";
+import VueRouter from "vue-router";
 import Notes from "../views/notes/notes.vue"
 import Home from "../views/home/home.vue"
 import Games from "../views/games/games.vue"
-import Tictactoe from "../views/tictactoe/tictactoe.vue"
-
+import Apps from "../views/apps/apps.vue"
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component:Home
+    component: Home
   },
   {
     path: "/notes",
     name: "notes",
-    component:Notes
+    component: Notes
   }
-  // ,
-  // {
-  //   path: "/games",
-  //   name: "games",
-  //   component:Tictactoe
-  // }
+  ,
+  {
+    path: "/games",
+    name: "games",
+    component: Games
+  },
+  {
+    path: "/apps",
+    name: "apps",
+    component: Apps
+  }
 ];
 
 const router = new VueRouter({
