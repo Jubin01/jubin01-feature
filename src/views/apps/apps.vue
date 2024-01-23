cef3c6658bd8c42cda044a674bd7efb38e6083db
jubin thomas <athom130@adient.com>
2024-01-23 20:19:58 +0530

    embed tictactoe game

---
+++ src/views/apps/apps.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="apps">
+    <h3>Apps</h3>
+    😴
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Apps',
+  data: () => ({
+  }),
+  mounted() {
+  }
+};
+
+</script>
--- src/views/games/games.vue
+++ src/views/games/games.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="games">
     <h3>Games</h3>
-    
 
+
+    &nbsp<a>  TicTacToe</a>
+    <div>
+      <embed src="https://ttt-fe.web.app" style="width:400px; height: 400px;">
+    </div>
   </div>
 </template>
 
