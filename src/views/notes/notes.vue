60433739cbcec46e0378efbb263df7de293d7fbc
jubin thomas <athom130@adient.com>
2024-06-15 12:59:31 +0530

    ##embed notepad app to show notes

--- src/views/notes/notes.vue
+++ src/views/notes/notes.vue
@@ -1,25 +1,14 @@
 <template>
-  <div class="notes">
-    <h3>Notes</h3>
-    <div v-for="(value, index) in notes" :key="index">
-      <h6>{{ value.date }}, source: <a :href=value.source target="_blank">
-          <img :alt=value.source :src=value.icon height="16" :title=value.source /></a></h6>
-      <h4>{{ value.note }} </h4>
-      <br />
-    </div>
-
-  </div>
+  <embed src="https://notes-fe.web.app/" style="width:400px; height: 400px;">
 </template>
 
 <script>
-import notes from "../../../public/assets/notes.json";
 export default {
   name: 'Notes',
   data: () => ({
-    notes: []
+
   }),
   mounted() {
-    this.notes = notes.notes;
   }
 };
 
